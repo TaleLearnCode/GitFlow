95016db2eda39f772a77f77028e0a8dd4f75a3d9
Chad Green <chad.green@jasperengines.com>
2024-01-08 09:48:02 -0500

    Adding failing test

--- tests/GitFlow.Services.Tests/Services/HelloWorldServicesTests.cs
+++ tests/GitFlow.Services.Tests/Services/HelloWorldServicesTests.cs
@@ -18,4 +18,21 @@ public class HelloWorldServicesTests
 		Assert.Equal($"Hello World from {environmentName} with version {environmentVersion}", result);
 	}
 
+	[Fact]
+	public void GetHelloWorld_Fails()
+	{
+
+		// Arrange
+		string environmentName = "Test Environment";
+		string environmentVersion = "1.0";
+		HelloWorldServices helloWorldServices = new(environmentName, "2.0");
+
+		// Act
+		string result = helloWorldServices.GetHelloWorld();
+
+		// Assert
+		Assert.Equal($"Hello World from {environmentName} with version {environmentVersion}", result);
+
+	}
+
 }
