5213573146bbc6e696817be9badf83561366d332
Chad Green <chad.green@jasperengines.com>
2024-01-08 09:26:11 -0500

    Updating the version number

--- src/GitFlow.Functions/Program.cs
+++ src/GitFlow.Functions/Program.cs
@@ -1,10 +1,8 @@
-using JET.GitFlow.Services;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 string environmentName = Environment.GetEnvironmentVariable("EnvironmentName")!;
-string environmentVersion = "1.0.0";
+string environmentVersion = "1.0.1";
 
 
 IHost host = new HostBuilder()
